e014f6e8957e7b5c7b4611cbb726554ed845f895
Adam Lockwood <alockwood@clearvision-cm.com>
2016-10-11 16:55:15 +0100

    wip

--- src/motg/src/footer.php
+++ src/motg/src/footer.php
@@ -8,8 +8,16 @@
 	?>
 
 	<div id="copyright">
-		<p>&copy; <?php echo date("Y").'  '.bloginfo("name").' | <a href="mailto:'.antispambot("michaeladamlockwood@googlemail.com?Subject=Hello").'" title="Contact e-mail address" target="_blank">moc.liamelgoog@doowkcolmadaleahcim</a></p>';
+		<!--<p>
+		<?php
+		//echo date("Y").'  '.bloginfo("name").' | <a href="mailto:'.antispambot("michaeladamlockwood@googlemail.com?Subject=Hello").'" title="Contact e-mail address" target="_blank">moc.liamelgoog@doowkcolmadaleahcim</a></p>';
 		?>
+		<?php
+		global $blog_id;
+		echo $blod_id;
+		//$current_blog_details = get_blog_details( $blog_id );
+//echo $current_blog_details->blogname;
+		echo bloginfo("name").' Is An ExEx.events event'; ?>
 	</div>
 
 </div><!-- // wrap -->
